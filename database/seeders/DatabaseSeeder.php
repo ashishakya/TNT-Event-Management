87d264f09f9d25996bb8ec1bdd5877c902c6a27a
Ashish Shakya <ashishakya@outlook.com>
2024-07-15 22:02:56 +0545

    Add model and migration for Event

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,8 +22,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call(
             [
-                        EventSeeder::class,
-                    ]
+                EventSeeder::class,
+            ]
         );
     }
 }
